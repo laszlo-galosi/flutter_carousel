cb3428048b6aaa30672a83255ce064345bf0bb98
László Gálosi <laszlo.galosi78@gmail.com>
2018-12-30 23:12:01 +0100

    Added carousel widget with asset images.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 const _materialPadding = EdgeInsets.all(16.0);
+
+const _imageNames = [917971, 965986, 900890, 836945, 941223, 952679];
+
+Widget _testCarousel = new Container(
+    child: new CarouselSlider(
+        items: _imageNames.map((i) {
+          return new Builder(
+            builder: (BuildContext context) {
+              return new Container(
+                  width: MediaQuery.of(context).size.width,
+//                    margin: new EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: new BoxDecoration(color: Colors.lime),
+                  child: new Image(
+                      image: new AssetImage('images/$i.jpg'),
+                      width: MediaQuery.of(context).size.width,
+                      height: 1080.0,
+                      fit: BoxFit.cover));
+            },
+          );
+        }).toList(),
+        height: 800.0,
+        autoPlay: true));
+
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
+
+  return result;
+}
+
+final List _carouselWidthIndicator = map<Widget>(_imageNames, (index, i) {
+  return Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(
+            children: <Widget>[
+              new Image(
+                  image: new AssetImage('images/$i.jpg'),
+                  width: 1920.0,
+                  height: 1080.0,
+                  fit: BoxFit.cover),
+              Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(200, 0, 0, 0),
+                          Color.fromARGB(0, 0, 0, 0)
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      )),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        '#${index + 1} - $i.jpg',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ))),
+            ],
+          )));
+}).toList();
 
 void main() => runApp(MyApp());
 
@@ -45,17 +117,67 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CarouselWithIndicator extends StatefulWidget {
+  CarouselWithIndicator({Key key, this.autoPlay}) : super(key: key);
 
-  void _incrementCounter() {
+  bool autoPlay = false;
+
+  @override
+  _CarouselWithIndicatorState createState() => _CarouselWithIndicatorState();
+}
+
+class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
+  int _current = 0;
+
+  bool _isAuto = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      CarouselSlider(
+        items: _carouselWidthIndicator,
+        autoPlay: _isAuto,
+        aspectRatio: 2.0,
+        updateCallback: (index) {
+          setState(() {
+            _current = index;
+          });
+        },
+      ),
+      Container(
+          //top: 0.0,
+          //left: 0.0,
+          //right: 0.0,
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: map<Widget>(_imageNames, (index, name) {
+          return Container(
+            width: 8.0,
+            height: 8.0,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _current == index
+                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                    : Color.fromRGBO(0, 0, 0, 0.4)),
+          );
+        }),
+      ))
+    ]);
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _autoPlay = true;
+
+  void toggleAutoPlay() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _autoPlay = !_autoPlay;
     });
   }
 
@@ -69,56 +191,22 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the MyHomePage object that was createdP bPy
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: _materialPadding,
-              child: Text(
-                'You have clicked the button this many times:',
-                style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.normal),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(
-                  fontSize: 44.0,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: CarouselWithIndicator(autoPlay: _autoPlay)
+          //Padding(
+          //  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          //  child: _testCarousel,
+          //),
+
+          ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: toggleAutoPlay,
+        tooltip: 'Autoplay',
+        child: Icon(Icons.play_arrow),
+      ),
     );
   }
 }
