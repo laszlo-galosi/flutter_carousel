38a44534974086621e2de2d36b1a8483b7194cfd
laszlo_galosi <laszlo.galosi78@gmail.com>
2019-01-03 16:27:45 +0100

    Added splash screen to android.

--- android/app/src/main/kotlin/hu/extremenet/fluttercarousel/MainActivity.kt
+++ android/app/src/main/kotlin/hu/extremenet/fluttercarousel/MainActivity.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity(), OnGlobalLayoutListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //make transparent status bar
+        getWindow().setStatusBarColor(0x00000000)
+        GeneratedPluginRegistrant.registerWith(this@MainActivity)
+
+        getFlutterView().viewTreeObserver.addOnGlobalLayoutListener(this)
+    }
+
+    override fun onGlobalLayout() {
+        //Remove full screen flag after load
+        getFlutterView().viewTreeObserver.removeOnGlobalLayoutListener(this)
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
 }
